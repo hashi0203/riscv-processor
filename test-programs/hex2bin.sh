7c2cc89a4ea9cfee71ad3398b9fec44d9730690b
hashi0203 <n.hsmt1123@gmail.com>
2021-07-28 15:38:00 +0900

    update hex2bin

--- test-programs/hex2bin.sh
+++ test-programs/hex2bin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while read line; do
 	printf '32'\''b'
-	echo "obase=2; ibase=16; ${line}" | bc | awk '{printf "%32s", $0}' | sed -e 's/ /0/g'
+	echo "obase=2; ibase=16; ${line}" | bc | printf "%32s" $(cat) | sed -e 's/ /0/g'
 	echo ','
 done < $1
